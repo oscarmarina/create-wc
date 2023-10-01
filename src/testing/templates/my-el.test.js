9d260f4958bc9e93ba421802b2e464058962d1ad
oscar marina <oscar7600@outlook.com>
2023-10-01 19:05:00 +0200

    feat: enhancements in Type Checking

--- src/testing/templates/my-el.test.js
+++ src/testing/templates/my-el.test.js
@@ -1,15 +1,17 @@
-import { html, fixture, assert, fixtureCleanup } from '@open-wc/testing';
-
+import { html, fixture, assert, expect, fixtureCleanup } from '@open-wc/testing';
 import '../define/<%= tagName %>.js';
 
 suite('<%= className %>', () => {
+  /**
+   * @type {import('../index').<%= className %>}
+   */
   let el;
 
   teardown(() => fixtureCleanup());
 
   suite('Default', () => {
     setup(async () => {
-      el = await fixture(html` <<%= tagName %>>light-dom</<%= tagName %>> `);
+      el = await fixture(html`<<%= tagName %>>light-dom</<%= tagName %>>`);
       await el.updateComplete;
     });
 
@@ -20,11 +22,11 @@ suite('<%= className %>', () => {
 
     suite('Semantic Dom and a11y', () => {
       test('SHADOW DOM - Structure test', async () => {
-        await assert.shadowDom.equalSnapshot(el, { ignoreAttributes: ['id'] });
+        await expect(el).shadowDom.to.equalSnapshot();
       });
 
       test('LIGHT DOM - Structure test', async () => {
-        await assert.lightDom.equalSnapshot(el, { ignoreAttributes: ['id'] });
+        await expect(el).lightDom.to.equalSnapshot();
       });
       test('a11y', async () => {
         await assert.isAccessible(el);
@@ -34,19 +36,19 @@ suite('<%= className %>', () => {
 
   suite('Events ', () => {
     setup(async () => {
-      el = await fixture(html` <<%= tagName %>></<%= tagName %>> `);
+      el = await fixture(html`<<%= tagName %>></<%= tagName %>>`);
       await el.updateComplete;
     });
 
     test('increases the counter on button click', () => {
-      el.shadowRoot.querySelector('button').click();
+      el.shadowRoot?.querySelector('button')?.click();
       assert.equal(el.counter, 6);
     });
   });
 
   suite('Override ', () => {
     setup(async () => {
-      el = await fixture(html` <<%= tagName %> heading="attribute heading"></<%= tagName %>> `);
+      el = await fixture(html`<<%= tagName %> heading="attribute heading"></<%= tagName %>>`);
       await el.updateComplete;
     });
 
