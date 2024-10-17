694008fc033f6f7fc1577742f8df08a4ebcf006f
oscar marina <oscar7600@outlook.com>
2024-10-17 15:35:50 +0200

    feat: update eslint version

--- src/common-repo/templates/eslint.config.js
+++ src/common-repo/templates/eslint.config.js
@@ -1,13 +1,20 @@
+// @ts-nocheck
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import importPlugin from 'eslint-plugin-import';
+import {configs as wc} from 'eslint-plugin-wc';
+import {configs as lit} from 'eslint-plugin-lit';
 import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
 import html from '@html-eslint/eslint-plugin';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import path from 'node:path';
 import js from '@eslint/js';
-import {fileURLToPath} from 'node:url';
 import {FlatCompat} from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const fileTypes = '{js,ts,mjs}';
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
@@ -15,21 +22,72 @@ const compat = new FlatCompat({
   allConfig: js.configs.all,
 });
 
-const htmlFilesConfig = [html.configs['flat/recommended']].map((conf) => ({
+// eslint-plugin-import
+const importFilesConfig = [
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+].map((conf) => ({
   ...conf,
-  files: ['**/*.html'],
+  files: [`**/*.${fileTypes}`],
+  languageOptions: {
+    parser: tsParser,
+  },
 }));
 
-const htmlFilesRules = {
-  files: ['**/*.html'],
+const importFilesRules = {
+  files: [`**/*.${fileTypes}`],
   rules: {
-    '@html-eslint/indent': ['error', 2],
-    '@html-eslint/require-closing-tags': 'off',
-    '@html-eslint/no-extra-spacing-attrs': 'off',
-    '@html-eslint/attrs-newline': 'off',
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        ignorePackages: true,
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          `**/test/**/*.${fileTypes}`,
+          `**/*.config.${fileTypes}`,
+          `**/*.conf.${fileTypes}`,
+        ],
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
   },
 };
 
+// eslint-plugin-wc
+const wcFilesConfig = [wc['flat/recommended']].map((conf) => ({
+  ...conf,
+  files: [`**/*.${fileTypes}`],
+}));
+
+const wcFilesRules = {
+  files: [`**/*.${fileTypes}`],
+  rules: {
+    'wc/no-constructor-attributes': 'warn',
+  },
+};
+
+// eslint-plugin-lit
+const litFilesConfig = [lit['flat/recommended']].map((conf) => ({
+  ...conf,
+  files: [`**/*.${fileTypes}`],
+}));
+
+const litFilesRules = {
+  files: [`**/*.${fileTypes}`],
+  rules: {
+    'lit/lifecycle-super': 'error',
+    'lit/no-native-attributes': 'off',
+    'lit/no-useless-template-literals': 'off',
+  },
+};
+
+// @html-eslint/typescript-eslint
 const tsFilesConfig = [...tseslint.configs.strict, ...tseslint.configs.stylistic].map((conf) => ({
   ...conf,
   files: ['**/*.ts'],
@@ -47,34 +105,41 @@ const tsFilesRules = {
   },
 };
 
+// @html-eslint/eslint-plugin
+const htmlFilesConfig = [html.configs['flat/recommended']].map((conf) => ({
+  ...conf,
+  files: ['**/*.html'],
+}));
+
+const htmlFilesRules = {
+  files: ['**/*.html'],
+  rules: {
+    '@html-eslint/indent': ['error', 2],
+    '@html-eslint/require-closing-tags': 'off',
+    '@html-eslint/no-extra-spacing-attrs': 'off',
+    '@html-eslint/attrs-newline': 'off',
+  },
+};
+
+// eslint-config
 export default [
   {
     ignores: [
-      '**/.idea',
-      '**/.vscode',
-      '**/coverage',
-      '**/reports',
-      '**/__snapshots__',
-      '**/screenshots',
-      '**/_site',
-      '**/dist',
-      '**/dev',
-      '**/build',
-      '**/.tmp',
-      '**/.wireit',
-      '**/*.d.ts',
-      '**/storybook-static',
-      '**/*.config.*',
-      '**/*.workspace.*',
-      '**/*.min.js',
-      '**/*-styles.*',
+      '**/{.idea,.vscode,coverage,reports,__snapshots__,screenshots,_site,dist,dev,build,.tmp,.wireit,storybook-static}/**',
+      '**/*.{d.ts,min.js,config.*,workspace.*,conf.*,test/**/*}',
     ],
   },
-  ...compat.extends('@open-wc'),
-  ...htmlFilesConfig,
-  htmlFilesRules,
+  ...compat.extends('plugin:lit-a11y/recommended'),
+  ...importFilesConfig,
+  importFilesRules,
+  ...wcFilesConfig,
+  wcFilesRules,
+  ...litFilesConfig,
+  litFilesRules,
   ...tsFilesConfig,
   tsFilesRules,
+  ...htmlFilesConfig,
+  htmlFilesRules,
   eslintConfigPrettier,
   {
     languageOptions: {
@@ -99,34 +164,14 @@ export default [
           ignoredNodes: ['PropertyDefinition', 'TemplateLiteral *'],
         },
       ],
-      'import/extensions': [
-        'error',
-        'always',
-        {
-          ignorePackages: true,
-        },
-      ],
-      'import/no-extraneous-dependencies': [
-        'error',
-        {
-          devDependencies: [
-            '**/test/**/*.{js,ts}',
-            '**/*.config.{js,ts,mjs,cjs}',
-            '**/*.conf.{js,ts,mjs,cjs}',
-          ],
-        },
-      ],
-      'import/no-unresolved': 'off',
-      'import/prefer-default-export': 'off',
-      'lit/no-native-attributes': 'off',
-      'lit/no-useless-template-literals': 'off',
+      'no-empty-function': 'error',
+      'lit-a11y/no-autofocus': 'off',
       'lit-a11y/anchor-is-valid': 'off',
       'lit-a11y/click-events-have-key-events': 'off',
-      'lit-a11y/no-autofocus': 'off',
     },
   },
   {
-    files: ['**/test/**/*.{js,ts}'],
+    files: [`**/test/**/*.${fileTypes}`],
     rules: {
       'no-unused-expressions': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
