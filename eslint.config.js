f38ba159dc4a8f798d26cb847c82e46005334418
oscar marina <oscar7600@outlook.com>
2025-10-25 11:59:01 +0200

    chore: update version

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js';
+import openwcEslintConfig from '@open-wc/eslint-config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['**/src/*/templates/**/*.js'],
+  },
+  js.configs.recommended,
+  ...openwcEslintConfig,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/src/**/*.html'],
+    rules: {
+      'import-x/no-unresolved': 'off',
+    },
+  },
+  {
+    files: ['**/test/**/*.js', '**/*.config.js'],
+    rules: {
+      'no-console': 'off',
+      'no-unused-expressions': 'off',
+      'class-methods-use-this': 'off',
+      'max-classes-per-file': 'off',
+    },
+  },
+];
